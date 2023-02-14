d54feab8e65552855987f32c61e32718ff813821
heudersena <documento_arquivo@hotmail.com>
2023-02-14 17:35:18 -0300

    criando regras de login do usuario

--- LotoBackEnd/database/seeders/DatabaseSeeder.php
+++ LotoBackEnd/database/seeders/DatabaseSeeder.php
@@ -14,11 +14,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        \App\Models\User::factory(1000)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        \App\Models\User::factory()->create([
+            'name' => 'Heuder Rodrigues de Sena',
+            'email' => 'hsena@gmail.com',
+        ]);
     }
 }
